71ec8c1d637075d3c32f04512d67e2f19f43203d
jose.calderon <jose.calderon@tranzact.net>
2022-11-13 14:38:45 -0500

    intento subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProcesadorComponent } from './pages/procesador/procesador.component';
-
+import { ThemingService, ThemingModule } from '@fundamental-ngx/core/theming';
+import { RouterModule } from '@angular/router';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { PlatformDynamicPageModule } from '@fundamental-ngx/platform/dynamic-page';
+import { BarModule } from '@fundamental-ngx/core/bar';
+import { ToolbarModule } from '@fundamental-ngx/core/toolbar';
+import { PlatformButtonModule } from '@fundamental-ngx/platform/button';
+import { DynamicPageModule } from '@fundamental-ngx/core/dynamic-page';
+import { ButtonModule } from '@fundamental-ngx/core/button';
+import { IconModule } from '@fundamental-ngx/core/icon';
 @NgModule({
   declarations: [AppComponent, ProcesadorComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    PlatformDynamicPageModule,
+    BarModule,
+    PlatformButtonModule,
+    ToolbarModule,
+    ButtonModule,
+    IconModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ThemingModule.withConfig({ defaultTheme: 'sap_horizon_dark' }),
+    RouterModule.forRoot([]),
+    NoopAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(themingService: ThemingService) {
+    themingService.init();
+  }
+}
